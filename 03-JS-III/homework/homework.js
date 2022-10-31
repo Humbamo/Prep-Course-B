543fefb0ac58c8613ee5bd879e6b799109a57ab5
Humberto Barragan Moreno <hbarraganm@gmail.com>
2022-10-31 14:43:35 -0500

    homework 3 complete

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,38 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0
+    const numeros = [1,2,3,4,5,6,7,8,9,10];
+    for(const e of numeros) suma += e
+    return suma 
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  let par = array.filter(n => n%2 === 0)
+  return par 
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map ((num) => num ** 2);
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((prev, next) => prev + next, array [0]);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+return num.toString().length
 }
   
   
